4c8a731aafb0bfbb6725d101b30f5d2399236632
naveen <naveenkumar9707@gmail.com>
2024-06-01 13:25:37 -0500

    copying roboshop.conf file

--- frontend.sh
+++ frontend.sh
@@ -1,4 +1,5 @@
 dnf install nginx -y
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 systemctl enable nginx
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
