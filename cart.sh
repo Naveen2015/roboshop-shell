848b3afe949cf2a9174089f7fc409f46c3719b02
naveen <naveenkumar9707@gmail.com>
2024-06-08 13:12:33 -0500

    mongodb script sed change

--- cart.sh
+++ cart.sh
@@ -2,11 +2,11 @@ dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
 dnf install nodejs -y
 useradd roboshop
+cp cart.service /etc/systemd/system/cart.service
 mkdir /app
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
 unzip /tmp/cart.zip
-cd /app
 npm install
 systemctl daemon-reload
 systemctl enable cart
--- payment.sh
+++ payment.sh
@@ -1,10 +1,10 @@
 dnf install python36 gcc python3-devel -y
 useradd roboshop
+cp payment.service /etc/systemd/system/payment.service
 mkdir /app
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
 cd /app
 unzip /tmp/payment.zip
-cd /app
 pip3.6 install -r requirements.txt
 systemctl daemon-reload
 systemctl enable payment
