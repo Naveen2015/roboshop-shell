6c54c7393eff577c5988c8af138c7bcfecc32f25
naveen <naveenkumar9707@gmail.com>
2024-06-08 17:02:48 -0500

    redis script

--- redis.sh
+++ redis.sh
@@ -1,5 +1,11 @@
+echo -e "\e[32m Configuring Redis and Installing\e[0m"
 dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 dnf module enable redis:remi-6.2 -y
 dnf install redis -y
+
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis/redis.conf
+
+echo -e "\e[32m Starting Redis\e[0m"
 systemctl enable redis
 systemctl start redis
