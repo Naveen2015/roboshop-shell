92578f320127acda94d53827012058b85dd90c18
naveen <naveenkumar9707@gmail.com>
2024-06-08 16:31:20 -0500

    roboshop.conf

--- catalogue.sh
+++ catalogue.sh
@@ -1,16 +1,30 @@
+echo -e "\e[36m configuring and installing Nodejs \e[0m"
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
 dnf install nodejs -y
+
+echo -e "\e[36m Add Application User\e[0m"
 useradd roboshop
+echo -e "\e[36m Create Application Directory \e[0m"
+rm -rf /app
 mkdir /app
-cp catalogue.service /etc/systemd/system/catalogue.service
+
+echo -e "\e[36m Download App Content\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
+echo -e "\e[36m unzip App Content\e[0m"
+
+
 unzip /tmp/catalogue.zip
+
+echo -e "\e[36m copy catalogue service file to systemd\e[0m"
+cp catalogue.service /etc/systemd/system/catalogue.service
+
+echo -e "\e[36m Download App Content\e[0m"
 
 npm install
 systemctl daemon-reload
 systemctl enable catalogue
-systemctl start catalogue
+systemctl restart catalogue
 dnf install mongodb-org-shell -y
 mongo --host mongod-dev.kruthikadevops.online	</app/schema/catalogue.js
