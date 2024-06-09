a75eb57d9c9c2a57bdd6a7ebf3da2d8cc5f1e33c
naveen <naveenkumar9707@gmail.com>
2024-06-09 08:26:37 -0500

    mysql and shipping scripts

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,10 @@
+echo -e "\e[32m Installing mysql\e[0m"
 dnf module disable mysql -y
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+cp /home/centos/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
 dnf install mysql-community-server -y
+
+echo -e "\e[32m staring mysql \e[0m"
 systemctl enable mysqld
-systemctl start mysqld
+systemctl restart mysqld
+echo -e "\e[32m Reset mysql password \e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
