31a7babb04f789fa33b0db99c79e6f7a584f14bf
naveen <naveenkumar9707@gmail.com>
2024-06-22 08:51:38 -0500

    overall changes

--- mongo.sh
+++ mongo.sh
@@ -1,6 +1,6 @@
 script=$(realpath $0)
 script_path=$(dirname $script)
-source script_path/common.sh
+source  ${script_path}/common.sh
 
 func_print_heading "copying mongo repo file to yum repository"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
